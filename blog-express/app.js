f1b96d0935efe3c197c3cab12507eebb0e1899e7
DingAllen <DingAllen@users.noreply.github.com>
2020-03-16 15:35:56 +0800

    day 7

--- blog-express/app.js
+++ blog-express/app.js
@@ -1,11 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const fs = require('fs')
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session')
+const RedisStore = require('connect-redis')(session)
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
 const blogRouter = require('./routes/blog')
 const userRouter = require('./routes/user')
@@ -16,24 +19,56 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const ENV = process.env.NODE_ENV
+if (ENV === 'production') {
+  // 线上环境
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+  const logFileName = path.join(__dirname, 'logs', 'access.log')
+  const writeStream = fs.createWriteStream(logFileName, {
+    flags: 'a'
+  })
+  app.use(logger('combined', {
+    stream: writeStream
+  }))
+} else {
+  app.use(logger('dev'));
+}
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+const redisClient = require('./db/redis')
+const sessionStore = new RedisStore({
+  client: redisClient
+})
+app.use(session({
+  secret: 'lunjingyi',
+  cookie: {
+    path: '/',
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000
+  },
+  store: sessionStore
+}))
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 app.use('/api/blog', blogRouter);
 app.use('/api/user', userRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- blog-express/routes/user.js
+++ blog-express/routes/user.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const {
+    login
+} = require('../controller/user')
+const {
+    SuccessModel,
+    ErrorModel
+} = require('../model/resModel')
 
 /* GET home page. */
 router.post('/login', (req, res, next) => {
@@ -7,13 +14,34 @@ router.post('/login', (req, res, next) => {
         username,
         password
     } = req.body
-    res.json({
-        errno: 0,
-        data: {
-            username,
-            password
+
+    const result = login(username, password)
+    return result.then(data => {
+        if (data.username) {
+            // 设置 session
+            req.session.username = data.username
+            req.session.realname = data.realname
+
+            res.json(new SuccessModel())
+            return
         }
-    });
+        res.json(new ErrorModel('登录失败'))
+    })
 });
+
+router.get('/login-test', (req, res, next) => {
+
+    if (req.session.username) {
+        res.json({
+            errno: 0,
+            msg: '测试成功'
+        })
+        return
+    }
+    res.json({
+        errno: -1,
+        msg: '测试失败'
+    })
+})
 
 module.exports = router;
